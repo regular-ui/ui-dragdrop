175ed218a1c005ae8424258d1243f88b5807cc16
Yusen <rainforest92@126.com>
2015-12-01 18:30:15 +0800

    [changed] drag & drop by mouse events

--- src/dragDrop.js
+++ src/dragDrop.js
@@ -1,10 +1,13 @@
 var dragDrop = {
+    dragging: false,
     data: null,
-    cancel: false,
+    proxy: null,
     screenX: 0,
     screenY: 0,
     movementX: 0,
-    movementY: 0
+    movementY: 0,
+    droppable: null,
+    droppables: []
 }
 
 module.exports = dragDrop;
--- src/draggable.js
+++ src/draggable.js
@@ -7,21 +7,20 @@
 
 'use strict';
 
+var Component = require('regular-ui-base/src/component');
 var _ = require('regular-ui-base/src/_');
 var dragDrop = require('./dragDrop.js');
 
-var isFirefox = navigator.userAgent.indexOf('Firefox') >= 0;
-
 /**
  * @class Draggable
- * @extend Regular
+ * @extend Component
  * @param {object}                  options.data                     =  绑定属性
- * @param {string|Dragable.Image|function='auto'}  options.data.image  @=> 拖拽时的图像。值为`auto`拖拽时，复制一个拖拽元素的图像（浏览器的默认设置），值为`empty`拖拽时不显示图像，可以用`<draggable.image>`自定义图像。也可以直接传入一个函数，要求返回{image: 图像元素, x: 横向偏移, y: 纵向偏移}。
+ * @param {string|Dragable.Proxy|function='auto'}  options.data.proxy  @=> 拖拽时的图像。值为`auto`拖拽时，复制一个拖拽元素的图像（浏览器的默认设置），值为`empty`拖拽时不显示图像，可以用`<draggable.proxy>`自定义图像。也可以直接传入一个函数，要求返回{proxy: 图像元素, x: 横向偏移, y: 纵向偏移}。
  * @param {string}                  options.data.effect              => 效果（与浏览器的effectAllowed一致）。可选的值有`none`、`uninitialized`、`all`、`copy`、`move`、`link`、`copyLink`、`copyMove`、`linkMove`。
  * @param {object}                  options.data.data                => 拖拽时需要传递的数据
  * @param {boolean=false}           options.data.disabled            => 是否禁用
  */
-var Draggable = Regular.extend({
+var Draggable = Component.extend({
     name: 'draggable',
     template: '{#inc this.$body}',
     /**
@@ -29,168 +28,224 @@ var Draggable = Regular.extend({
      */
     config: function() {
         _.extend(this.data, {
-            image: 'auto',
-            effect: undefined,
-            data: null,
-            disabled: false
+            proxy: 'clone',
+            direction: 'all',
+            data: null
         });
         this.supr();
+
+        this._onMouseDown = this._onMouseDown.bind(this);
+        this._onBodyMouseMove = this._onBodyMouseMove.bind(this);
+        this._onBodyMouseUp = this._onBodyMouseUp.bind(this);
     },
     init: function() {
-        // 修改内部DOM元素
         var inner = _.dom.element(this);
-        _.dom.on(inner, 'dragstart', this._onDragStart.bind(this));
-        _.dom.on(inner, 'drag', this._onDrag.bind(this));
-        _.dom.on(inner, 'dragend', this._onDragEnd.bind(this));
-
-        this._onDragOverDocument = this._onDragOverDocument.bind(this);
+        _.dom.on(inner, 'mousedown', this._onMouseDown);
+        this.supr();
 
         this.$watch('disabled', function(newValue) {
-            inner.draggable = !newValue;
-
             if(newValue)
                 _.dom.delClass(inner, 'z-draggable');
             else
                 _.dom.addClass(inner, 'z-draggable');
-
         });
-        this.supr();
     },
-    _getImageData: function() {
-        if(typeof this.data.image === 'function')
-            return this.data.image();
-        else if(this.data.image instanceof Draggable.Image) {
-            var image = _.dom.element(this.data.image);
-            return {
-                image: image,
-                x: this.data.image.data.x,
-                y: this.data.image.data.y,
-                appendToBody: true
-            };
-        } else if(this.data.image === 'empty') {
-            var empty = document.createElement('span');
-            empty.innerHTML = '&nbsp;';
-            empty.style.position = 'fixed';
-            return {
-                image: empty,
-                x: 0, y: 0,
-                appendToBody: true
-            };
-        } else if(this.data.image === 'auto')
-            return null;
+    _getProxy: function() {
+        if(typeof this.data.proxy === 'function')
+            return this.data.proxy();
+        else if(this.data.proxy instanceof HTMLElement)
+            return this.data.proxy;
+        else if(this.data.proxy instanceof Draggable.Proxy) {
+            var proxy = _.dom.element(this.data.proxy);
+            var dimension = _.dom.getDimension(_.dom.element(this));
+            this._initProxy(proxy, dimension);
+            return proxy;
+        } else if(this.data.proxy === 'clone') {
+            var proxy = _.dom.element(this);
+            var dimension = _.dom.getDimension(proxy);
+            proxy = proxy.cloneNode(true);
+            this._initProxy(proxy, dimension);
+            return proxy;
+        } else if(this.data.proxy === 'self') {
+            var proxy = _.dom.element(this);
+            var dimension = _.dom.getDimension(proxy);
+            this._initProxy(proxy, dimension);
+            return proxy;
+        }
     },
-    _onDragStart: function($event) {
-        var e = $event.event;
+    _initProxy: function(proxy, dimension) {
+        proxy.style.left = dimension.left + 'px';
+        proxy.style.top = dimension.top + 'px';
+        proxy.style.zIndex = '2000';
+        proxy.style.position = 'fixed';
+        document.body.appendChild(proxy);
+    },
+    _onMouseDown: function($event) {
+        if(this.data.disabled)
+            return;
+        $event.preventDefault();
 
-        // 处理DataTransfer
-        // IE低版本没有这个属性
-        if(e.dataTransfer) {
-            var imageData = this._getImageData();
-            if(imageData) {
-                imageData.appendToBody && document.body.appendChild(imageData.image);
-                e.dataTransfer.setDragImage(imageData.image, imageData.x, imageData.y);
+        _.dom.on(document.body, 'mousemove', this._onBodyMouseMove);
+        _.dom.on(document.body, 'mouseup', this._onBodyMouseUp);
+    },
+    _onBodyMouseMove: function($event) {
+        var e = $event.event;
+        $event.preventDefault();
+
+        if(dragDrop.dragging === false) {
+            _.extend(dragDrop, {
+                dragging: true,
+                data: this.data.data,
+                proxy: this._getProxy(),
+                screenX: e.screenX,
+                screenY: e.screenY,
+                movementX: 0,
+                movementY: 0,
+                droppable: undefined
+            }, true);
+
+            if(dragDrop.proxy)
+                _.dom.addClass(dragDrop.proxy, 'z-dragging');
+
+            this.$emit('dragstart', {
+                origin: this,
+                source: this,
+                target: dragDrop.proxy,
+                data: dragDrop.data,
+                screenX: dragDrop.screenX,
+                screenY: dragDrop.screenY,
+                movementX: dragDrop.movementX,
+                movementY: dragDrop.movementY
+            });
+        } else {
+            dragDrop.movementX = e.screenX - dragDrop.screenX;
+            dragDrop.movementY = e.screenY - dragDrop.screenY;
+            dragDrop.screenX = e.screenX;
+            dragDrop.screenY = e.screenY;
+
+            if(dragDrop.proxy) {
+                if(this.data.direction === 'all' || this.data.direction === 'horizontal')
+                    dragDrop.proxy.style.left = dragDrop.proxy.offsetLeft + dragDrop.movementX + 'px';
+                if(this.data.direction === 'all' || this.data.direction === 'vertical')
+                    dragDrop.proxy.style.top = dragDrop.proxy.offsetTop + dragDrop.movementY + 'px';
             }
 
-            if(this.data.effect)
-                e.dataTransfer.effectAllowed = this.data.effect;
+            this.$emit('drag', {
+                origin: this,
+                source: this,
+                target: dragDrop.proxy,
+                data: this.data.data,
+                screenX: dragDrop.screenX,
+                screenY: dragDrop.screenY,
+                movementX: dragDrop.movementX,
+                movementY: dragDrop.movementY
+            });
 
-            // Firefox必须设置这个东西
-            e.dataTransfer.setData('text', '');
+            // Drop
+            dragDrop.proxy.style.display = 'none';
+            var pointElement = document.elementFromPoint(e.clientX, e.clientY);
+            dragDrop.proxy.style.display = '';
+
+            var pointDroppable = dragDrop.droppables.find(function(droppable) {
+                return _.dom.element(droppable) === pointElement;
+            });
+
+            if(dragDrop.droppable !== pointDroppable) {
+                pointDroppable && pointDroppable.$emit('dragenter', {
+                    origin: this,
+                    source: pointDroppable,
+                    target: pointElement,
+                    data: this.data.data,
+                    screenX: dragDrop.screenX,
+                    screenY: dragDrop.screenY,
+                    movementX: dragDrop.movementX,
+                    movementY: dragDrop.movementY
+                });
+                dragDrop.droppable && dragDrop.droppable.$emit('dragleave', {
+                    origin: this,
+                    source: dragDrop.droppable,
+                    target: undefined,
+                    data: this.data.data,
+                    screenX: dragDrop.screenX,
+                    screenY: dragDrop.screenY,
+                    movementX: dragDrop.movementX,
+                    movementY: dragDrop.movementY
+                });
+
+                dragDrop.droppable = pointDroppable;
+            } else {
+                pointDroppable && pointDroppable.$emit('dragover', {
+                    origin: this,
+                    source: pointDroppable,
+                    target: pointElement,
+                    data: this.data.data,
+                    screenX: dragDrop.screenX,
+                    screenY: dragDrop.screenY,
+                    movementX: dragDrop.movementX,
+                    movementY: dragDrop.movementY
+                });
+            }
+        }
+    },
+    _onBodyMouseUp: function($event) {
+        $event.preventDefault();
+
+        if(dragDrop.droppable) {
+            dragDrop.droppable.data.data = this.data.data;
+            dragDrop.droppable.$update();
+
+            dragDrop.droppable.$emit('drop', {
+                origin: this,
+                source: dragDrop.droppable,
+                target: _.dom.element(dragDrop.droppable),
+                data: this.data.data,
+                screenX: dragDrop.screenX,
+                screenY: dragDrop.screenY,
+                movementX: 0,
+                movementY: 0
+            });
         }
 
-        dragDrop.data = this.data.data;
-        dragDrop.cancel = false;
-        dragDrop.movementX = 0;
-        dragDrop.movementY = 0;
-        dragDrop.screenX = e.screenX;
-        dragDrop.screenY = e.screenY;
-        dragDrop.imageData = imageData;
+        this.$emit('dragend', {
+            origin: this,
+            source: this,
+            target: undefined
+        });
 
-        isFirefox && _.dom.on(document.body, 'dragover', this._onDragOverDocument);
+        if(dragDrop.proxy) {
+            if(this.data.proxy instanceof Draggable.Proxy || this.data.proxy === 'clone')
+                document.body.removeChild(dragDrop.proxy);
 
-        // emit事件
-        var eventData = _.extend(_.extend({
-            data: dragDrop.data
-        }, $event), e);
-        this.$emit('dragstart', eventData);
+            _.dom.delClass(dragDrop.proxy, 'z-dragging');
+        }
 
-        _.dom.addClass(e.target, 'z-dragging');
+        this.cancel();
     },
-    _onDrag: function($event) {
-        var e = $event.event;
+    cancel: function() {
+        _.extend(dragDrop, {
+            dragging: false,
+            data: null,
+            proxy: null,
+            screenX: 0,
+            screenY: 0,
+            movementX: 0,
+            movementY: 0,
+            droppable: undefined
+        }, true);
 
-        // 拖拽结束时会监听到一个都为0的事件
-        if(e.clientX === 0 && e.clientY === 0 && e.screenX === 0 && e.screenY === 0)
-            return;
-
-        dragDrop.movementX = e.screenX - dragDrop.screenX;
-        dragDrop.movementY = e.screenY - dragDrop.screenY;
-        dragDrop.screenX = e.screenX;
-        dragDrop.screenY = e.screenY;
-
-        // emit事件
-        var eventData = _.extend(_.extend({
-            data: dragDrop.data,
-            movementX: dragDrop.movementX,
-            movementY: dragDrop.movementY
-        }, $event), e);
-        this.$emit('drag', eventData);
-    },
-    _onDragEnd: function($event) {
-        var e = $event.event;
-
-        isFirefox && _.dom.off(document.body, 'dragover', this._onDragOverDocument);
-
-        _.dom.delClass(e.target, 'z-dragging');
-        if(dragDrop.imageData && dragDrop.imageData.appendToBody)
-            document.body.removeChild(dragDrop.imageData.image);
-
-        dragDrop.data = null;
-        dragDrop.cancel = false;
-        dragDrop.imageData = null;
-
-        var eventData = _.extend(_.extend({}, $event), e);
-        this.$emit('dragend', eventData);
-    },
-    // For FireFox
-    _onDragOverDocument: function($event) {
-        var e = $event.event;
-
-        // 拖拽结束时会监听到一个都为0的事件
-        if(e.clientX === 0 && e.clientY === 0 && e.screenX === 0 && e.screenY === 0)
-            return;
-
-        dragDrop.movementX = e.screenX - dragDrop.screenX;
-        dragDrop.movementY = e.screenY - dragDrop.screenY;
-        dragDrop.screenX = e.screenX;
-        dragDrop.screenY = e.screenY;
-
-        // emit事件
-        var eventData = _.extend(_.extend({
-            data: dragDrop.data,
-            movementX: dragDrop.movementX,
-            movementY: dragDrop.movementY
-        }, $event), e);
-        this.$emit('drag', eventData);
+        _.dom.off(document.body, 'mousemove', this._onBodyMouseMove);
+        _.dom.off(document.body, 'mouseup', this._onBodyMouseUp);
     }
 });
 
-Draggable.Image = Regular.extend({
-    name: 'draggable.image',
+Draggable.Proxy = Regular.extend({
+    name: 'draggable.proxy',
     template: '{#inc this.$body}',
-    config: function() {
-        _.extend(this.data, {
-            x: 0,
-            y: 0
-        });
-        this.supr();
-    },
     init: function() {
-        var image = _.dom.element(this);
-        image.style.position = 'fixed';
         if(this.$outer instanceof Draggable)
-            this.$outer.data.image = this;
+            this.$outer.data.proxy = this;
     }
+    // node: _.noop
 })
 
 module.exports = Draggable;
