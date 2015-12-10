96de7803980f62e06930a1b4c06b5b5902010621
Yusen <rainforest92@126.com>
2015-12-10 23:16:20 +0800

    v0.1.6 of regular-ui

--- src/draggable.js
+++ src/draggable.js
@@ -15,9 +15,12 @@ var dragdrop = require('./dragdrop.js');
  * @class Draggable
  * @extend Component
  * @param {object}                  options.data                     =  绑定属性
- * @param {string|Dragable.Proxy|HTMLElement|function='clone'}  options.data.proxy  @=> 拖拽代理，即拖拽时显示的元素。默认值为`clone`，拖拽时拖起自身的一个拷贝；当值为`self`，拖拽时直接拖起自身。也可以用`<draggable.proxy>`自定义代理，或直接传入一个元素或函数。其他值表示不使用拖拽代理。
  * @param {object}                  options.data.data                => 拖拽时需要传递的数据
+ * @param {string|Dragable.Proxy|Element|function='clone'}  options.data.proxy  @=> 拖拽代理，即拖拽时显示的元素。默认值为`clone`，拖拽时拖起自身的一个拷贝；当值为`self`，拖拽时直接拖起自身。也可以用`<draggable.proxy>`自定义代理，或直接传入一个元素或函数。其他值表示不使用拖拽代理。
+ * @param {string='all'}            options.data.direction           => 拖拽代理可以移动的方向，`all`为任意方向，`horizontal`为水平方向，`vertical`为垂直方向
  * @param {boolean=false}           options.data.disabled            => 是否禁用
+ * @param {string='z-draggable'}    options.data.class               => 可拖拽时（即disabled=false）给元素附加此class
+ * @param {string='z-drag'}         options.data.dragClass           => 拖拽该元素时给元素附加此class
  */
 var Draggable = Component.extend({
     name: 'draggable',
@@ -27,35 +30,43 @@ var Draggable = Component.extend({
      */
     config: function() {
         _.extend(this.data, {
+            data: null,
             proxy: 'clone',
             direction: 'all',
-            data: null,
-            attachClass: true
+            'class': 'z-draggable',
+            dragClass: 'z-drag'
         });
         this.supr();
 
         this._onMouseDown = this._onMouseDown.bind(this);
         this._onBodyMouseMove = this._onBodyMouseMove.bind(this);
         this._onBodyMouseUp = this._onBodyMouseUp.bind(this);
+        this.cancel = this.cancel.bind(this);
     },
+    /**
+     * @protected
+     */
     init: function() {
         var inner = _.dom.element(this);
         _.dom.on(inner, 'mousedown', this._onMouseDown);
         this.supr();
 
         this.$watch('disabled', function(newValue) {
-            if(this.data.attachClass) {
-                if(newValue)
-                    _.dom.delClass(inner, 'z-draggable');
-                else
-                    _.dom.addClass(inner, 'z-draggable');
-            }
+            if(newValue)
+                _.dom.delClass(inner, this.data['class']);
+            else
+                _.dom.addClass(inner, this.data['class']);
         });
     },
+    /**
+     * @method _getProxy() 获取拖拽代理
+     * @private
+     * @return {Element} 拖拽代理元素
+     */
     _getProxy: function() {
         if(typeof this.data.proxy === 'function')
             return this.data.proxy();
-        else if(this.data.proxy instanceof HTMLElement)
+        else if(this.data.proxy instanceof Element)
             return this.data.proxy;
         else if(this.data.proxy instanceof Draggable.Proxy) {
             var proxy = _.dom.element(this.data.proxy);
@@ -77,6 +88,11 @@ var Draggable = Component.extend({
             return proxy;
         }
     },
+    /**
+     * @method _initProxy() 初始化拖拽代理
+     * @private
+     * @return {void}
+     */
     _initProxy: function(proxy, dimension) {
         proxy.style.left = dimension.left + 'px';
         proxy.style.top = dimension.top + 'px';
@@ -84,6 +100,9 @@ var Draggable = Component.extend({
         proxy.style.position = 'fixed';
         proxy.style.display = '';
     },
+    /**
+     * @private
+     */
     _onMouseDown: function($event) {
         if(this.data.disabled)
             return;
@@ -92,6 +111,9 @@ var Draggable = Component.extend({
         _.dom.on(document.body, 'mousemove', this._onBodyMouseMove);
         _.dom.on(document.body, 'mouseup', this._onBodyMouseUp);
     },
+    /**
+     * @private
+     */
     _onBodyMouseMove: function($event) {
         var e = $event.event;
         $event.preventDefault();
@@ -133,6 +155,8 @@ var Draggable = Component.extend({
             }
 
             this._drag();
+            if(!dragdrop.dragging)
+                return;
 
             // Drop
             dragdrop.proxy.style.display = 'none';
@@ -144,23 +168,35 @@ var Draggable = Component.extend({
             });
 
             if(dragdrop.droppable !== pointDroppable) {
-                dragdrop.droppable && this._dragLeave(dragdrop.droppable);
-                pointDroppable && this._dragEnter(pointDroppable);
+                dragdrop.droppable && dragdrop.droppable._dragLeave(this);
+                if(!dragdrop.dragging)
+                    return;
+                pointDroppable && pointDroppable._dragEnter(this);
+                if(!dragdrop.dragging)
+                    return;
                 dragdrop.droppable = pointDroppable;
             } else
-                pointDroppable && this._dragOver(pointDroppable);
+                pointDroppable && pointDroppable._dragOver(this);
         }
     },
+    /**
+     * @private
+     */
     _onBodyMouseUp: function($event) {
         var e = $event.event;
         $event.preventDefault();
 
-        dragdrop.droppable && this._drop(dragdrop.droppable);
-        this._dragEnd();
-
+        dragdrop.droppable && dragdrop.droppable._drop(this);
         this.cancel();
     },
+    /**
+     * @method cancel() 取消拖拽操作
+     * @public
+     * @return {void}
+     */
     cancel: function() {
+        this._dragEnd();
+
         _.extend(dragdrop, {
             dragging: false,
             data: null,
@@ -179,83 +215,90 @@ var Draggable = Component.extend({
         _.dom.off(document.body, 'mousemove', this._onBodyMouseMove);
         _.dom.off(document.body, 'mouseup', this._onBodyMouseUp);
     },
+    /**
+     * @private
+     */
     _dragStart: function(e) {
-        if(dragdrop.proxy && this.data.attachClass)
-            _.dom.addClass(dragdrop.proxy, 'z-dragging');
+        if(dragdrop.proxy)
+            _.dom.addClass(dragdrop.proxy, this.data.dragClass);
 
+        /**
+         * @event dragstart 拖拽开始时触发
+         * @property {object} source 事件发起对象，为当前draggable
+         * @property {object} target 事件目标对象，为拖拽代理元素
+         * @property {object} origin 事件源，即拖拽源，为当前draggable
+         * @property {object} data 拖拽时需要传递的数据
+         * @property {object} proxy 拖拽代理元素
+         * @property {number} screenX 鼠标指针相对于屏幕的水平位置
+         * @property {number} screenY 鼠标指针相对于屏幕的垂直位置
+         * @property {number} clientX 鼠标指针相对于浏览器的水平位置
+         * @property {number} clientY 鼠标指针相对于浏览器的垂直位置
+         * @property {number} pageX 鼠标指针相对于页面的水平位置
+         * @property {number} pageY 鼠标指针相对于页面的垂直位置
+         * @property {number} movementX 鼠标指针水平位置相对于上次操作的偏移量
+         * @property {number} movementY 鼠标指针垂直位置相对于上次操作的偏移量
+         * @property {function} cancel 取消拖拽操作
+         */
         this.$emit('dragstart', _.extend({
-            origin: this,
-            source: this,
-            target: dragdrop.proxy
-        }, dragdrop));
-    },
-    _drag: function() {
-        this.$emit('drag', _.extend({
-            origin: this,
             source: this,
             target: dragdrop.proxy,
+            origin: this,
+            proxy: dragdrop.proxy,
+            cancel: this.cancel
+        }, dragdrop));
+    },
+    /**
+     * @private
+     */
+    _drag: function() {
+        /**
+         * @event drag 正在拖拽时触发
+         * @property {object} source 事件发起对象，为当前draggable
+         * @property {object} target 事件目标对象，为拖拽代理元素
+         * @property {object} origin 事件源，即拖拽源，为当前draggable
+         * @property {object} data 拖拽时需要传递的数据
+         * @property {object} proxy 拖拽代理元素
+         * @property {number} screenX 鼠标指针相对于屏幕的水平位置
+         * @property {number} screenY 鼠标指针相对于屏幕的垂直位置
+         * @property {number} clientX 鼠标指针相对于浏览器的水平位置
+         * @property {number} clientY 鼠标指针相对于浏览器的垂直位置
+         * @property {number} pageX 鼠标指针相对于页面的水平位置
+         * @property {number} pageY 鼠标指针相对于页面的垂直位置
+         * @property {number} movementX 鼠标指针水平位置相对于上次操作的偏移量
+         * @property {number} movementY 鼠标指针垂直位置相对于上次操作的偏移量
+         * @property {function} cancel 取消拖拽操作
+         */
+        this.$emit('drag', _.extend({
+            source: this,
+            target: dragdrop.proxy,
+            origin: this,
+            proxy: dragdrop.proxy,
+            cancel: this.cancel
          }, dragdrop));
     },
-    _dragEnter: function(droppable) {
-        var element = _.dom.element(droppable);
-        droppable.data.attachClass && _.dom.addClass(element, 'z-dragover');
-        
-        droppable.$emit('dragenter', _.extend({
-            origin: this,
-            source: droppable,
-            target: element
-        }, dragdrop));
-    },
-    _dragLeave: function(droppable) {
-        var element = _.dom.element(droppable);
-        droppable.data.attachClass && _.dom.delClass(element, 'z-dragover');
-        
-        droppable.$emit('dragleave', _.extend({
-            origin: this,
-            source: droppable,
-            target: element
-        }, dragdrop));
-    },
-    _dragOver: function(droppable) {
-        var element = _.dom.element(droppable);
-        var dimension = _.dom.getDimension(element);
-
-        droppable.$emit('dragover', _.extend({
-            origin: this,
-            source: droppable,
-            target: element,
-            ratioX: (dragdrop.clientX - dimension.left)/dimension.width,
-            ratioY: (dragdrop.clientY - dimension.top)/dimension.height
-        }, dragdrop));
-    },
-    _drop: function(droppable) {
-        var element = _.dom.element(droppable);
-        droppable.data.attachClass && _.dom.delClass(element, 'z-dragover');
-        var dimension = _.dom.getDimension(element);
-
-        droppable.data.data = this.data.data;
-        droppable.$update();
-
-        droppable.$emit('drop', _.extend({
-            origin: this,
-            source: droppable,
-            target: element,
-            ratioX: (dragdrop.clientX - dimension.left)/dimension.width,
-            ratioY: (dragdrop.clientY - dimension.top)/dimension.height
-        }, dragdrop));
-    },
+    /**
+     * @private
+     */
     _dragEnd: function() {
+        /**
+         * @event dragend 拖拽结束时触发
+         * @property {object} source 事件发起对象，为当前draggable
+         * @property {object} target 事件目标对象，为拖拽代理元素
+         * @property {object} origin 事件源，即拖拽源，为当前draggable
+         * @property {object} proxy 拖拽代理元素
+         */
         this.$emit('dragend', {
-            origin: this,
             source: this,
-            target: dragdrop.proxy
+            target: dragdrop.proxy,
+            origin: this,
+            proxy: dragdrop.proxy
         });
 
         if(dragdrop.proxy) {
             if(this.data.proxy instanceof Draggable.Proxy || this.data.proxy === 'clone')
                 dragdrop.proxy.parentElement.removeChild(dragdrop.proxy);
 
-            this.data.attachClass && _.dom.delClass(dragdrop.proxy, 'z-dragging');
+            _.dom.delClass(dragdrop.proxy, this.data.dragClass);
         }
     }
 });
@@ -263,6 +306,9 @@ var Draggable = Component.extend({
 Draggable.Proxy = Component.extend({
     name: 'draggable.proxy',
     template: '{#inc this.$body}',
+    /**
+     * @protected
+     */
     init: function() {
         if(this.$outer instanceof Draggable) {
             _.dom.element(this).style.display = 'none';
--- src/droppable.js
+++ src/droppable.js
@@ -17,6 +17,8 @@ var dragdrop = require('./dragdrop.js');
  * @param {object}                  options.data                     =  绑定属性
  * @param {object}                  options.data.data               <=  拖放后传递过来的数据
  * @param {boolean=false}           options.data.disabled            => 是否禁用
+ * @param {string='z-droppable'}    options.data.class               => 可放置时（即disabled=false）给元素附加此class
+ * @param {string='z-dragover'}     options.data.dragOverClass       => 拖拽该元素上方时给元素附加此class
  */
 var Droppable = Component.extend({
     name: 'droppable',
@@ -27,27 +29,162 @@ var Droppable = Component.extend({
     config: function() {
         _.extend(this.data, {
             data: null,
-            attachClass: true
+            'class': 'z-droppable',
+            dragOverClass: 'z-dragover'
         });
         this.supr();
 
         dragdrop.droppables.push(this);
     },
+    /**
+     * @protected
+     */
     init: function() {
         var inner = _.dom.element(this);
         this.$watch('disabled', function(newValue) {
-            if(this.data.attachClass) {
-                if(newValue)
-                    _.dom.delClass(inner, 'z-droppable');
-                else
-                    _.dom.addClass(inner, 'z-droppable');
-            }
+            if(newValue)
+                _.dom.delClass(inner, this.data['class']);
+            else
+                _.dom.addClass(inner, this.data['class']);
         });
         this.supr();
     },
+    /**
+     * @protected
+     */
     destroy: function() {
         dragdrop.droppables.splice(dragdrop.droppables.indexOf(this), 1);
         this.supr();
+    },
+    /**
+     * @private
+     */
+    _dragEnter: function(origin) {
+        var element = _.dom.element(this);
+        _.dom.addClass(element, this.data.dragOverClass);
+        
+        /**
+         * @event dragenter 拖拽进入该元素时触发
+         * @property {object} source 事件发起对象，为当前droppable
+         * @property {object} target 事件目标对象，为当前接收元素
+         * @property {object} origin 事件源，即拖拽源，为拖拽的draggable
+         * @property {object} data 拖拽时接收到的数据
+         * @property {number} screenX 鼠标指针相对于屏幕的水平位置
+         * @property {number} screenY 鼠标指针相对于屏幕的垂直位置
+         * @property {number} clientX 鼠标指针相对于浏览器的水平位置
+         * @property {number} clientY 鼠标指针相对于浏览器的垂直位置
+         * @property {number} pageX 鼠标指针相对于页面的水平位置
+         * @property {number} pageY 鼠标指针相对于页面的垂直位置
+         * @property {number} movementX 鼠标指针水平位置相对于上次操作的偏移量
+         * @property {number} movementY 鼠标指针垂直位置相对于上次操作的偏移量
+         * @property {function} cancel 取消拖拽操作
+         */
+        this.$emit('dragenter', _.extend({
+            source: this,
+            target: element,
+            origin: origin,
+            cancel: origin.cancel
+        }, dragdrop));
+    },
+    /**
+     * @private
+     */
+    _dragLeave: function(origin) {
+        var element = _.dom.element(this);
+        _.dom.delClass(element, this.data.dragOverClass);
+        
+        /**
+         * @event dragleave 拖拽离开该元素时触发
+         * @property {object} source 事件发起对象，为当前droppable
+         * @property {object} target 事件目标对象，为当前接收元素
+         * @property {object} origin 事件源，即拖拽源，为拖拽的draggable
+         * @property {object} data 拖拽时接收到的数据
+         * @property {number} screenX 鼠标指针相对于屏幕的水平位置
+         * @property {number} screenY 鼠标指针相对于屏幕的垂直位置
+         * @property {number} clientX 鼠标指针相对于浏览器的水平位置
+         * @property {number} clientY 鼠标指针相对于浏览器的垂直位置
+         * @property {number} pageX 鼠标指针相对于页面的水平位置
+         * @property {number} pageY 鼠标指针相对于页面的垂直位置
+         * @property {number} movementX 鼠标指针水平位置相对于上次操作的偏移量
+         * @property {number} movementY 鼠标指针垂直位置相对于上次操作的偏移量
+         * @property {function} cancel 取消拖拽操作
+         */
+        this.$emit('dragleave', _.extend({
+            source: this,
+            target: element,
+            origin: origin,
+            cancel: origin.cancel
+        }, dragdrop));
+    },
+    /**
+     * @private
+     */
+    _dragOver: function(origin) {
+        var element = _.dom.element(this);
+        var dimension = _.dom.getDimension(element);
+
+        /**
+         * @event dragover 拖拽在该元素上方时触发
+         * @property {object} source 事件发起对象，为当前droppable
+         * @property {object} target 事件目标对象，为当前接收元素
+         * @property {object} origin 事件源，即拖拽源，为拖拽的draggable
+         * @property {object} data 拖拽时接收到的数据
+         * @property {number} ratioX 鼠标指针相对于接收元素所占的长度比
+         * @property {number} ratioY 鼠标指针相对于接收元素所占的高度比
+         * @property {number} screenX 鼠标指针相对于屏幕的水平位置
+         * @property {number} screenY 鼠标指针相对于屏幕的垂直位置
+         * @property {number} clientX 鼠标指针相对于浏览器的水平位置
+         * @property {number} clientY 鼠标指针相对于浏览器的垂直位置
+         * @property {number} pageX 鼠标指针相对于页面的水平位置
+         * @property {number} pageY 鼠标指针相对于页面的垂直位置
+         * @property {number} movementX 鼠标指针水平位置相对于上次操作的偏移量
+         * @property {number} movementY 鼠标指针垂直位置相对于上次操作的偏移量
+         * @property {function} cancel 取消拖拽操作
+         */
+        this.$emit('dragover', _.extend({
+            source: this,
+            target: element,
+            origin: origin,
+            ratioX: (dragdrop.clientX - dimension.left)/dimension.width,
+            ratioY: (dragdrop.clientY - dimension.top)/dimension.height,
+            cancel: origin.cancel
+        }, dragdrop));
+    },
+    /**
+     * @private
+     */
+    _drop: function(origin) {
+        var element = _.dom.element(this);
+        _.dom.delClass(element, this.data.dragOverClass);
+        var dimension = _.dom.getDimension(element);
+
+        this.data.data = origin.data.data;
+        this.$update();
+
+        /**
+         * @event drop 拖拽放置时触发
+         * @property {object} source 事件发起对象，为当前droppable
+         * @property {object} target 事件目标对象，为当前接收元素
+         * @property {object} origin 事件源，即拖拽源，为拖拽的draggable
+         * @property {object} data 拖拽时接收到的数据
+         * @property {number} ratioX 鼠标指针相对于接收元素所占的长度比
+         * @property {number} ratioY 鼠标指针相对于接收元素所占的高度比
+         * @property {number} screenX 鼠标指针相对于屏幕的水平位置
+         * @property {number} screenY 鼠标指针相对于屏幕的垂直位置
+         * @property {number} clientX 鼠标指针相对于浏览器的水平位置
+         * @property {number} clientY 鼠标指针相对于浏览器的垂直位置
+         * @property {number} pageX 鼠标指针相对于页面的水平位置
+         * @property {number} pageY 鼠标指针相对于页面的垂直位置
+         * @property {number} movementX 鼠标指针水平位置相对于上次操作的偏移量
+         * @property {number} movementY 鼠标指针垂直位置相对于上次操作的偏移量
+         */
+        this.$emit('drop', _.extend({
+            source: this,
+            target: element,
+            origin: origin,
+            ratioX: (dragdrop.clientX - dimension.left)/dimension.width,
+            ratioY: (dragdrop.clientY - dimension.top)/dimension.height
+        }, dragdrop));
     }
 });
 
